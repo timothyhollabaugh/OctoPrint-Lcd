7f8079aef6f851e4c95aa0ebcf64e4233772b529
Tim Hollabaugh <haytowntim@embarqmail.com>
2015-11-18 18:50:53 -0500

    Fixed scroll lag

--- octoprint_lcd/ui/files.py
+++ octoprint_lcd/ui/files.py
@@ -8,16 +8,21 @@ import octoprint.server as Server
 
 class FilesTab(BoxLayout):
     file_list=ObjectProperty(None)
+
+    first = True
+
     def __init__(self, **kwargs):
         super(FilesTab, self).__init__(**kwargs)
 
     def update(self, dt):
-        files = GridLayout(cols=1, spacing=10, size_hint_y=None)
-        # Make sure the height is such that there is something to scroll.
-        files.bind(minimum_height=files.setter('height'))
-        for i in range(30):
-            btn = Button(text=str(i), size_hint_y=None, height=40)
-            files.add_widget(btn)
-        scroll = ScrollView(size_hint=(None, None), size=(400, 400))
-        scroll.add_widget(files)
-        self.ids.file_list.add_widget(scroll)
+        if self.first:
+            files = GridLayout(cols=1, spacing=10, size_hint_y=None)
+            # Make sure the height is such that there is something to scroll.
+            files.bind(minimum_height=files.setter('height'))
+            for i in range(30):
+                btn = Button(text=str(i), size_hint_y=None, height=40)
+                files.add_widget(btn)
+            scroll = ScrollView(size_hint=(None, None), size=(400, 400))
+            scroll.add_widget(files)
+            self.ids.file_list.add_widget(scroll)
+            self.first = False
