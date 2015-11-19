255faeb3cc97513a8ff160f848612af5f926d1bd
Tim Hollabaugh <haytowntim@embarqmail.com>
2015-11-18 19:01:42 -0500

    moved scroll to kv

--- octoprint_lcd/ui/files.py
+++ octoprint_lcd/ui/files.py
@@ -16,13 +16,17 @@ class FilesTab(BoxLayout):
 
     def update(self, dt):
         if self.first:
-            files = GridLayout(cols=1, spacing=10, size_hint_y=None)
-            # Make sure the height is such that there is something to scroll.
-            files.bind(minimum_height=files.setter('height'))
+            # files = GridLayout(cols=1, spacing=10, size_hint_y=None)
+            # # Make sure the height is such that there is something to scroll.
+            # files.bind(minimum_height=files.setter('height'))
+            # for i in range(30):
+            #     btn = Button(text=str(i), size_hint_y=None, height=40)
+            #     files.add_widget(btn)
+            # scroll = ScrollView(size_hint=(None, None), size=(400, 400))
+            # scroll.add_widget(files)
+            # self.ids.file_list.add_widget(scroll)
+            self.ids.file_list.bind(minimum_height=self.ids.file_list.setter('height'))
             for i in range(30):
                 btn = Button(text=str(i), size_hint_y=None, height=40)
-                files.add_widget(btn)
-            scroll = ScrollView(size_hint=(None, None), size=(400, 400))
-            scroll.add_widget(files)
-            self.ids.file_list.add_widget(scroll)
+                self.ids.file_list.add_widget(btn)
             self.first = False
