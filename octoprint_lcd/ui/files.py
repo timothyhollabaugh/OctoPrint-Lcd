c4ddd452fa83397741faa9da1b399f8e1022eda2
Tim Hollabaugh <haytowntim@embarqmail.com>
2015-11-19 21:14:11 -0500

    Files sort by date uploaded

--- octoprint_lcd/ui/files.py
+++ octoprint_lcd/ui/files.py
@@ -9,6 +9,8 @@ from kivy.uix.behaviors import ToggleButtonBehavior
 
 from kivy.graphics import *
 
+import time
+
 import octoprint.server as Server
 
 class FileView(ToggleButtonBehavior, BoxLayout):
@@ -23,7 +25,6 @@ class FileView(ToggleButtonBehavior, BoxLayout):
         #print file
 
     def changeState(self, button, state):
-        print self.title, state
         if state == 'normal':
             self.setBackground(0.4, 0.4, 0.4)
         elif state == 'down':
@@ -50,13 +51,53 @@ class FilesTab(BoxLayout):
             self.ids.file_list.bind(minimum_height=self.ids.file_list.setter('height'))
             self.first = False
 
-        if(self.files != self.oldFiles):
+        if self.files != self.oldFiles:
             print "different"
             print self.oldFiles
             self.ids.file_list.clear_widgets()
+
+            #dates = [2, 3, 1, 5, 4]
+
             for i in self.files['local']:
+            #for i in dates:
                 btn = FileView(self.files['local'][i], size_hint_y=None, height=60)
-                for w in range(self.ids.file_list.children.size)
-                    if self.files[min(w-1, 0)]
-                self.ids.file_list.add_widget(btn)
+                #btn = FileView({'name': 'Test', 'date': i}, size_hint_y=None, height=60)
+                children = self.ids.file_list.children
+                date = self.files['local'][i]['date']
+                #date = i
+                #print len(children)
+                #print "Date:", date
+
+                if len(children) > 0:
+                    #print "Child:", children[0].date
+                    if date <= int(children[0].date) :
+                        #print "First:", date, children[0].date
+                        #print date <= children[0].date
+                        self.ids.file_list.add_widget(btn, index=0)
+                        #for c in children:
+                        #    print c.date
+                        #time.sleep(1)
+                    elif date > int(children[len(children)-1].date):
+                        print "Second:", date , children[len(children)-1].date
+                        self.ids.file_list.add_widget(btn, index=len(children))
+                        #for c in children:
+                        #    print c.date
+                        #time.sleep(1)
+                    else:
+                        #print "Third: "
+                        for w in range(len(children)):
+                            w = w
+                            #print "w: ", w
+                            if date <= int(children[w-1].date) and date >= int(children[w].date):
+                                #print date, children[w-1].date, children[w].date
+                                self.ids.file_list.add_widget(btn, index = w-1)
+                                #for c in children:
+                                #    print c.date
+                                #time.sleep(1)
+                else:
+                    self.ids.file_list.add_widget(btn)
+                    for c in children:
+                        print c.date
+                    time.sleep(1)
+
             self.oldFiles = self.files
