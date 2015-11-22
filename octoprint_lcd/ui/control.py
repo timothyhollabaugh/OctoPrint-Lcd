0a4de7e1752197c2527de831d518d057ba66c2ed
Tim Hollabaugh <haytowntim@embarqmail.com>
2015-11-21 20:25:20 -0500

    Added control.py

---
+++ octoprint_lcd/ui/control.py
@@ -0,0 +1,29 @@
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.behaviors import ToggleButtonBehavior
+
+import octoprint.server as Server
+
+class ControlTab(FloatLayout):
+    def update(self, dt):
+        pass
+
+    def jog(self, axis, mult):
+        self.selected = None
+        #print "Jogging", axis
+        if 'x' in axis or 'y' in axis:
+            step = 0
+            for f in ToggleButtonBehavior.get_widgets('xystep'):
+                if f.state == 'down':
+                    step = int(f.text)
+                    break
+            #print step*mult
+            Server.printer.jog(axis, step*mult)
+
+        if 'z' in axis:
+            step = 0
+            for f in ToggleButtonBehavior.get_widgets('zstep'):
+                if f.state == 'down':
+                    step = int(f.text)
+                    break
+            #print step*mult
+            Server.printer.jog(axis, step*mult)
