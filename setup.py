16840d0750f65002f8be8f760faea6f5a11ea9fa
Tim Hollabaugh <haytowntim@embarqmail.com>
2015-12-19 21:21:44 -0500

    Update version number

--- setup.py
+++ setup.py
@@ -14,11 +14,11 @@ plugin_package = "octoprint_lcd"
 plugin_name = "OctoPrint-Lcd"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.1.0"
+plugin_version = "1.2.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = """TODO"""
+plugin_description = """Displays a full user interface on a touchscreen"""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = "Tim H"
